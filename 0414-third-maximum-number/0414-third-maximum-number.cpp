0e15dcac9f7f14fa57a5f71e676586e860613f90
Krati-Kanodia <kratikanodia12.kk12@gmail.com>
2026-09-24 22:56:06 +0530

    Time: 0 ms (100%), Space: 12.8 MB (74.35%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+        
+        for (int num : nums) {
+            
+            // Ignore duplicates
+            if (num == first || num == second || num == third)
+                continue;
+            
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if (num > second) {
+                third = second;
+                second = num;
+            }
+            else if (num > third) {
+                third = num;
+            }
+        }
+        
+    
+        if (third == LLONG_MIN)
+            return first;
+        
+        return third;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
